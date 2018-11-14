b2e02e00e0c07ddd004577a24945b7e21371c29d
xiaobai <>
2018-11-14 09:40:37 +0800

    案例一，springmvc入门

---
+++ MavenExample1/src/main/java/com/nihuo/HelloSpringController.java
@@ -0,0 +1,18 @@
+package com.nihuo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+@Controller
+public class HelloSpringController {
+	String message = "Welcom to spring MVC";
+	@RequestMapping("/hello")
+	public ModelAndView showMessage(@RequestParam(value = "name", required = true) String name) {
+			ModelAndView mv = new ModelAndView("hellospring");//指定视图
+			//向视图中添加所要展示或使用的内容，将在页面中使用
+	        mv.addObject("message", message);
+	        mv.addObject("name", name);
+	        return mv;
+	}
+}
